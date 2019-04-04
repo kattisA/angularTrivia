f34203eba394738b98886e663a3ba77439495add
Katarina Lejonlid <katarina.lejonlid@aurorainnovation.com>
2019-04-04 10:55:48 +0200

    Added app routes file

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+	{
+	    path: '',
+	    component: CategoriesComponent,
+		  resolve: {
+		    data: CategoriesResolver
+			}
+	},
+	{
+	    path: 'questions/about/:categorySlug',
+	    component: CategoryQuestionsComponent,
+	    resolve: {
+	      data: CategoryQuestionsResolver
+	  	}
+	},
+	{
+	    path: 'question/:questionSlug',
+	    component: QuestionAnswersComponent,
+	    resolve: {
+	      data: QuestionAnswersResolver
+	  	}
+	}
+];
