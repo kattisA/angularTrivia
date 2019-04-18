21a184f185b810f2b41a1df5d5fd3bb134a84f00
Kattis <kattis.a@gmail.com>
2019-04-18 12:00:53 +0200

    Continued making some cards that now will show on first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularTrivia';
+  title = 'Angular Notes';
+
+  public cards: Array<any> = [
+    {text: 'Card 1'},
+    {text: 'Card 2'},
+    {text: 'Card 3'},
+    {text: 'Card 4'},
+    {text: 'Card 5'},
+    {text: 'Card 6'},
+    {text: 'Card 7'},
+    {text: 'Card 8'},
+    {text: 'Card 9'},
+    {text: 'Card 10'},
+  ];
+  
 }
